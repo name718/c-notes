9c41d779dd3a29efda748b90fac8e4a6cc717303
majuntao <majuntao_v@didiglobal.com>
2026-03-26 20:01:56 +0800

    实现二分查找算法，完成搜索功能并返回目标值的下标

--- leetcode/binary_search.c
+++ leetcode/binary_search.c
@@ -24,6 +24,21 @@ int search(int *nums, int numsSize, int target)
     (void)target;
 
     /* TODO: 在这里完成具体实现 */
+    int left = 0;
+    int right = numsSize - 1;
+    
+    while(left <= right){
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        }
+        if (target < nums[mid]) {
+            right = mid - 1;
+        }
+        if (target > nums[mid]) {
+            left = mid + 1;
+        }
+    }
     return -1;
 }
 
