fa41b834a1fcbdfc9bef61891f6f9159aa246f8d
majuntao <majuntao_v@didiglobal.com>
2026-03-30 15:05:01 +0800

    新增生成每种字符都是奇数个的字符串功能及示例代码

---
+++ leetcode/generateTheString.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+// 生成每种字符都是奇数个的字符串
+// 给你一个整数 n，请你返回一个含 n 个字符的字符串，其中每种字符在该字符串中都恰好出现 奇数次 。
+// 返回的字符串必须只含小写英文字母。如果存在多个满足题目要求的字符串，则返回其中任意一个即可。
+
+char* generateTheString(int n) {
+    // 开辟空间：n个字符 + 1个\0
+    char* res = (char*)malloc(n + 1);
+    
+    // 奇数：全a
+    if (n % 2 == 1) {
+        for (int i = 0; i < n; i++) {
+            res[i] = 'a';
+        }
+    }
+    // 偶数：n-1个a + 1个b
+    else {
+        for (int i = 0; i < n-1; i++) {
+            res[i] = 'a';
+        }
+        res[n-1] = 'b';
+    }
+
+    res[n] = '\0';  // 必须加结束符
+    return res;
+}
+
+int main(void) {
+    // 🟢 正确用法：n 必须 >= 1
+    int n = 5;
+    char* str = generateTheString(n);
+    printf("%s\n", str);
+    free(str);   // 这里一定安全
+
+    n = 4;
+    str = generateTheString(n);
+    printf("%s\n", str);
+    free(str);
+
+    return 0;
+}
